2fd231f814c29cc5252e4f621874322c938988da
KHodow677 <kylehodowany@utexas.edu>
2022-09-08 08:51:50 -0500

    Add Limit Order Function

--- Orders.py
+++ Orders.py
@@ -1,6 +1,6 @@
 import requests, os, json
 
-def market_order(symbol, quanitity, side, tif, endpoint, headers):
+def market_order(symbol, quanitity, endpoint, headers, side = "buy", tif = "day"):
     ord_url = endpoint + "/v2/orders"
     params = {"symbol" : symbol,
               "quantity" : quanitity,
@@ -9,4 +9,18 @@ def market_order(symbol, quanitity, side, tif, endpoint, headers):
               "time_in_force" : tif}
     r = requests.post(ord_url, headers = headers, json = params)
     return r.json()
+
+# Example call: market_order("AMZN", 1, endpoint, headers)
+
+def limit_order(symbol, quanitity, limit_pr, endpoint, headers, side = "buy", tif = "day"):
+    ord_url = endpoint + "/v2/orders"
+    params = {"symbol" : symbol,
+              "quantity" : quanitity,
+              "side" : side,
+              "type" : "market",
+              "limit_price" : limit_pr,
+              "time_in_force" : tif}
+    r = requests.post(ord_url, headers = headers, json = params)
+    return r.json()
     
+# Example call: market_order("AMZN", 1, 320, endpoint, headers)

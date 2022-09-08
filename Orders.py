303ad367b718dee8826f41c6c257a471ce7422d8
KHodow677 <kylehodowany@utexas.edu>
2022-09-08 08:44:18 -0500

    Update Market Order Function

--- Orders.py
+++ Orders.py
@@ -1,11 +1,12 @@
 import requests, os, json
 
-def market_order(symbol, quanitity, side, tif, endpoint):
+def market_order(symbol, quanitity, side, tif, endpoint, headers):
     ord_url = endpoint + "/v2/orders"
     params = {"symbol" : symbol,
               "quantity" : quanitity,
               "side" : side,
               "type" : "market",
               "time_in_force" : tif}
-    r = requests.post()
+    r = requests.post(ord_url, headers = headers, json = params)
+    return r.json()
     

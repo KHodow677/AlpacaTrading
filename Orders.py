e35d99d8d7c4720ad22e5772f36d0bcc5261aba0
KHodow677 <kylehodowany@utexas.edu>
2022-11-07 12:03:42 -0600

    Listing and Cancelling Orders

--- Orders.py
+++ Orders.py
@@ -1,4 +1,19 @@
-import requests, os, json
+import requests, os, json, pandas as pd
+
+def order_list(endpoint, headers, status = "open", limit = 50):
+    ord_list_url = endpoint + "/orders"
+    params = {"status":status}
+    r = requests.get(ord_list_url, headers=headers, params=params)
+    data = r.json()
+    return pd.DataFrame(data)
+
+def order_cancel(endpoint, headers, order_id=""):
+    if len(order_id)>1:
+        ord_cncl_url = endpoint + "/orders/{}".format(order_id)
+    else:
+        ord_cncl_url = endpoint + "/orders"
+    r = requests.delete(ord_cncl_url, headers=headers)
+    return r.json()
 
 # Example call: market_order("AMZN", 1, endpoint, headers)
 def market_order(symbol, quantity, endpoint, headers, side="buy", tif="day"):
@@ -50,7 +65,7 @@ def stop_limit_order(symbol, quanitity, stop_price, limit_price, endpoint, heade
 
 # Example call: stop_limit_order("AMZN", 1, 2, endpoint, headers, "sell") 
 def trail_stop_order(symbol, quantity, trail_pr, endpoint, headers, side="buy", tif="day"):
-    ord_url = endpoint + "/v2/orders"
+    ord_url = endpoint + "/orders"
     params = {"symbol": symbol,
               "qty": quantity,
               "side" : side,
@@ -62,7 +77,7 @@ def trail_stop_order(symbol, quantity, trail_pr, endpoint, headers, side="buy", 
 
 # Example call: stop_limit_order("AMZN", 1, 2, endpoint, headers, "sell") 
 def bracket_order(symbol, quantity, tplp, slsp, sllp, endpoint, headers, side="buy", tif="day"):
-    ord_url = endpoint + "/v2/orders"
+    ord_url = endpoint + "/orders"
     params = {
               "symbol": symbol,
               "qty": quantity,
